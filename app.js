49b526fe0b22206669c98d083c4945e1335158dc
Deepanshu Kumar <deepskmr822@gmail.com>
2024-03-16 01:34:08 +0530

    Episode-1

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ *
+ *<div id="parent">
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *</div>
+ *
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  // this returns an object
+  "h1",
+  { id: "heading" },
+  "Hello world from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root); // this is also an object
+
+root.render(parent);
